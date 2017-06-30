145f0bbeb3b4ddae96cd4b73b91f12be9f698000
王敬博 <470665207@qq.com>
2017-06-30 20:38:34 +0800

    更新服务代码

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
+const static = require('koa-static');
 const views = require('koa-views');
+const port = 6006;
+app.use(static(__dirname + '/storybook-static'));
 app.use(views(__dirname + '/storybook-static', {
   extension: 'ejs'
 }));
 app.use(async (ctx) => {
   await ctx.render('index.html');
 });
-app.listen(3000, () => {
-  console.log('server is listening: 3000')
+app.listen(port, () => {
+  console.log(`server is listening:${port}`);
 });
